c57a51c578d9d49a7c97b09f3f00f5e3813fb3e8
Code-Me <ecabanig22@gmail.com>
2018-11-06 23:02:48 +0800

    optimized for offline browsing

--- job-order-form.php
+++ job-order-form.php
@@ -1,6 +1,16 @@
 <?php
 require 'dbcon.php';
 
+if(isset($_GET['serial'])){
+    $sId = $_GET['serial'];
+    echo $sId;
+    $sql = "";  
+}
+
+
+
+
+
 if(isset($_POST['jos'])){
   //nameofoffice
   //serial
@@ -179,10 +189,10 @@ $conn->close();
     <title>Job Order Form</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="w3.css">
     <link rel="stylesheet" type="text/css" href="css/custom.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -240,7 +250,7 @@ require 'navbar.php';
                                 }
                               ?>
                             </select>
-<<<<<<< HEAD
+
         </div>
         <div class="row">
                              <h4 class="col-10"><b>Name of Office:</b>&nbsp;
@@ -253,7 +263,7 @@ require 'navbar.php';
 <div class="container" style="margin-top: ;">
     <!-- UPDATE form -->
     <form action="" method="POST">
-        <!-- UPDATE card -->
+        <!-- UPDATE card -->    
         <div class="card">
             <div class="card-body" style="margin-left:2%;">
 
--- job-order-view.php
+++ job-order-view.php
@@ -64,7 +64,7 @@ require'navbar.php';
 
                                   while ($row =  $result->fetch_assoc()) {
                                     echo "<tr>";
-                        echo "<td>" . $row['SerialCode'] . "</td>";
+                        echo "<td><a href='job-order-form.php?serial=". $row['SerialCode']. "'>" . $row['SerialCode'] . "</td>";
                         echo "<td>" . $row['Name'] . "</td>";
                         echo "<td>" . $row['DateRequestCreated'] . "</td>";
                         echo "<td>" . $row['statusId'] . "</td>";
