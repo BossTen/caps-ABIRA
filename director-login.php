84d31a4a4857f700ceeff380fb7fd3d409c8fff0
BossTen <tenthequeen@gmail.com>
2018-11-11 11:25:36 +0800

    logins added

---
+++ director-login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Head GSO Login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="w3.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/navbar.css">
+   <script src="js/search.js"></script>
+</head>
+<img src="logo.png" width="100%" height="20%">
+<br><br>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">    <h1 class="w3-text-red w3-center">GSO Director Log In</h1>
+    <center>
+             <form action="" method="post">
+               <div class="form-group"><input type="text" class="form-control" name="username" placeholder="Enter Username"></div>
+               <div class="form-group"><input type="password" class="form-control" name="password" placeholder="Password"></div>
+               <input type="submit" class="btn btn-success w3-xlarge" name="Login" value="Login">
+              </form>
+             </form>
+            </div>
+  </div>
+</div>
