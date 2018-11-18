0cfe6f71ae631c6edef9e8fe232e358c1e49104b
Code-Me <ecabanig22@gmail.com>
2018-11-19 02:49:20 +0800

    updated database

--- abira.sql
+++ abira.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2018 at 11:39 AM
+-- Generation Time: Nov 18, 2018 at 07:47 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.8
 
@@ -21,6 +21,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `abira`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `Id` int(10) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `password` varchar(350) NOT NULL,
+  `designation` varchar(150) NOT NULL,
+  `campus` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`Id`, `username`, `password`, `designation`, `campus`) VALUES
+(1, 'admin_alangilan', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'ALANGILAN'),
+(2, 'director_alangilan', '21232f297a57a5a743894a0e4a801fc3', 'director', 'ALANGILAN');
 
 -- --------------------------------------------------------
 
@@ -162,7 +184,10 @@ CREATE TABLE `joborder` (
 INSERT INTO `joborder` (`Id`, `SerialCode`, `DateRequestCreated`, `Campus`, `NameOfOffice`, `AirCondition`, `CarpentryMasonry`, `ElectricalWorks`, `Painting`, `Plumbing`, `Welding`, `InspectionReport`, `JobRecommendation`, `MaterialsNeededId`, `RequestorSignature`, `RequestorName`, `RequestorDesignation`, `DateRequested`, `signatureOfInspector`, `InspectorName`, `InspectorDesignation`, `DateInspected`, `Approved`, `ApprovedBy`, `StartOfService`, `StartOfServiceTime`, `EndOfService`, `EndOfServiceTime`, `NoOfHours`, `Assessment`, `AccomplishedWork1`, `WorkDoneBy1`, `Signature1`, `AccomplishedWork2`, `WorkDoneBy2`, `Signature2`, `AccomplishedWork3`, `WorkDoneBy3`, `Signature3`, `AccomplishedWork4`, `WorkDoneBy4`, `Signature4`, `ConformeName`, `ConformeApproved`, `ConformeDateApproved`, `ResponseTime`, `AccuracyOfWork`, `Courtesy`, `QualityOfService`, `statusId`, `priorityId`, `UserJobDescription`, `materialsNeeded1`, `materialsNeeded2`, `materialsNeeded3`, `materialsNeeded4`, `materialsNeeded5`, `materialsNeeded6`, `materialsNeeded7`, `materialsNeeded8`, `materialsNeeded9`, `materialsNeeded10`) VALUES
 (18, 'RO18111601', '2018-11-16', 'ALANGILAN', 'College of Accountancy, Business, Economics and International Hospitality Management  ', '', '', '', NULL, '', 'checked', NULL, NULL, NULL, NULL, 'MAGNAYE, CHRISTINA ANCHETA', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 4, 2, 'test', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 (19, 'AL18111601', '2018-11-16', 'ALANGILAN', 'College of Accountancy, Business, Economics and International Hospitality Management  ', '', '', 'checked', NULL, '', '', NULL, NULL, NULL, NULL, 'MAGNAYE, CHRISTINA ANCHETA', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 4, 2, 'There has been an issue with room 204 electricity, please check', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(20, 'AL18111602', '2018-11-16', 'ALANGILAN', 'College of Accountancy, Business, Economics and International Hospitality Management  ', 'off', 'off', 'checked', NULL, 'off', 'off', '', '', NULL, '', 'MAGNAYE, CHRISTINA ANCHETA', '', '0000-00-00', '', '', '', '0000-00-00', 0, NULL, '0000-00-00', '01:00 AM', '0000-00-00', '01:00 AM', 0, 'notcompleted', '   ', '', '', '   ', '', '', '', '', '', '', '', '', '', 0, '0000-00-00', 0, 0, 0, 0, 2, 2, 'There has been an issue with room 204 electricity, please check', '', '', '', '', '', '', '', '', '', '');
+(20, 'AL18111602', '2018-11-16', 'ALANGILAN', 'College of Accountancy, Business, Economics and International Hospitality Management  ', 'off', 'off', 'checked', NULL, 'off', 'off', '', '', NULL, '', 'MAGNAYE, CHRISTINA ANCHETA', '', '0000-00-00', '', '', '', '0000-00-00', 0, NULL, '0000-00-00', '01:00 AM', '0000-00-00', '01:00 AM', 0, 'notcompleted', '   ', '', '', '   ', '', '', '', '', '', '', '', '', '', 0, '0000-00-00', 0, 0, 0, 0, 2, 2, 'There has been an issue with room 204 electricity, please check', '', '', '', '', '', '', '', '', '', ''),
+(21, 'NA18111801', '2018-11-18', 'ALANGILAN', 'College of Accountancy, Business, Economics and International Hospitality Management  ', '', '', '', NULL, 'checked', '', NULL, NULL, NULL, NULL, 'MAGNAYE, CHRISTINA ANCHETA', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 7, 4, 'try', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(22, 'NA18111802', '2018-11-18', 'ALANGILAN', 'College of Accountancy, Business, Economics and International Hospitality Management  ', '', '', '', NULL, 'checked', 'checked', NULL, NULL, NULL, NULL, 'MAGNAYE, CHRISTINA ANCHETA', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 7, 4, 'try01', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(23, 'NA18111803', '2018-11-18', 'ALANGILAN', 'College of Accountancy, Business, Economics and International Hospitality Management  ', 'checked', 'checked', 'checked', NULL, 'checked', 'checked', NULL, NULL, NULL, NULL, 'MAGNAYE, CHRISTINA ANCHETA', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 7, 4, 'capstone test', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -180,29 +205,24 @@ CREATE TABLE `preventive_maintenance` (
   `area` varchar(150) DEFAULT NULL,
   `dateStarted` date DEFAULT NULL,
   `dateEnded` date DEFAULT NULL,
-  `accomplishedBy` varchar(150) DEFAULT NULL
+  `accomplishedBy` varchar(150) DEFAULT NULL,
+  `type` varchar(250) DEFAULT NULL,
+  `maintainance_activity` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `preventive_maintenance`
 --
 
-INSERT INTO `preventive_maintenance` (`id`, `work`, `campus`, `month`, `college`, `floor`, `area`, `dateStarted`, `dateEnded`, `accomplishedBy`) VALUES
-(1, 'dfg', 'dfg', 'dfg', 'dfgg', 'qw', 'qqq', '0000-00-00', '0000-00-00', 'q'),
-(2, 'AIR-CONDITIONING', 'NASUGBU', 'January', 'College of Accountancy, Business, Economics and International Hospitality Management  ', 'Basement Floor', 'area', '2018-11-03', '2018-11-13', 'tenorio'),
-(3, 'AIR-CONDITIONING', 'NASUGBU', 'January', 'College of Accountancy, Business, Economics and International Hospitality Management  ', 'Basement Floor', 'area', '2018-11-03', '2018-11-13', 'tenorio'),
-(4, '', '', '', '', '', '', '0000-00-00', '0000-00-00', ''),
-(5, '', '', '', '', '', '', '0000-00-00', '0000-00-00', ''),
-(6, '', '', '', '', '', '', '0000-00-00', '0000-00-00', ''),
-(7, '', '', '', '', '', '', '0000-00-00', '0000-00-00', ''),
-(8, '', '', '', '', '', '', '0000-00-00', '0000-00-00', ''),
-(9, '', '', '', '', '', '', '0000-00-00', '0000-00-00', ''),
-(10, '', '', '', '', '', '', '0000-00-00', '0000-00-00', ''),
-(11, '', '', '', '', '', '', '0000-00-00', '0000-00-00', ''),
-(12, '', '', '', '', '', '', '0000-00-00', '0000-00-00', ''),
-(13, '', '', '', '', '', '', '0000-00-00', '0000-00-00', ''),
-(14, '', '', '', '', '', '', '0000-00-00', '0000-00-00', ''),
-(15, '', '', '', '', '', '', '0000-00-00', '0000-00-00', '');
+INSERT INTO `preventive_maintenance` (`id`, `work`, `campus`, `month`, `college`, `floor`, `area`, `dateStarted`, `dateEnded`, `accomplishedBy`, `type`, `maintainance_activity`) VALUES
+(28, '', '', '', '', '', NULL, '0000-00-00', '0000-00-00', '', NULL, NULL),
+(29, '', 'ALANGILAN', 'January', 'CEAFA (College of Engineering, Architecture  & Fine Arts) Bldg.', 'Basement Floor', NULL, '2018-11-14', '2018-11-10', 'sa', 'Door (Repainted; Lock/Knobs repaired/ replaced)', NULL),
+(30, '', 'ALANGILAN', 'January', 'CEAFA (College of Engineering, Architecture  & Fine Arts) Bldg.', 'Basement Floor', NULL, '0000-00-00', '0000-00-00', 'sa', 'Door (Repainted; Lock/Knobs repaired/ replaced)', NULL),
+(31, 'Building, Walls, Doors, Windows', 'ALANGILAN', 'January', 'CEAFA (College of Engineering, Architecture  & Fine Arts) Bldg.', 'Basement Floor', NULL, '0000-00-00', '0000-00-00', 'sa', 'Door (Repainted; Lock/Knobs repaired/ replaced)', NULL),
+(32, '', 'ALANGILAN', 'January', '', '', '', '2018-11-17', '0000-00-00', 'a', NULL, 'Generator Set (Test run; Check fuel consumption; Cleaning)'),
+(33, '', 'ALANGILAN', 'January', '', '', '', '2018-11-03', '0000-00-00', 'a', NULL, 'Generator Set (Test run; Check fuel consumption; Cleaning)'),
+(34, 'Generator Set/Fuel Container', 'ALANGILAN', 'January', '', '', '', '2018-11-17', '0000-00-00', 'aaaaaaaa', NULL, 'Generator Set (Test run; Check fuel consumption; Cleaning)'),
+(35, 'Generator Set/Fuel Container', 'ALANGILAN', 'January', '', '', '', '2018-11-17', '0000-00-00', 'aaaaaaaa', NULL, 'Generator Set (Test run; Check fuel consumption; Cleaning)');
 
 -- --------------------------------------------------------
 
@@ -243,17 +263,23 @@ CREATE TABLE `status` (
 --
 
 INSERT INTO `status` (`Id`, `Name`, `Color`) VALUES
-(1, 'For Approval', NULL),
+(1, 'Pending For Approval', NULL),
 (2, 'Approved', NULL),
 (3, 'Denied', NULL),
 (4, 'Pending', NULL),
 (5, 'On-Going', NULL),
 (6, 'Completed', NULL),
-(7, 'For CSO Additional Info', NULL);
+(7, 'For Inspection', NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indexes for table `admin`
@@ -300,6 +326,12 @@ ALTER TABLE `status`
 --
 
 --
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
@@ -315,13 +347,13 @@ ALTER TABLE `director`
 -- AUTO_INCREMENT for table `joborder`
 --
 ALTER TABLE `joborder`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `preventive_maintenance`
 --
 ALTER TABLE `preventive_maintenance`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `priority`
