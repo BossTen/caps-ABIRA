b3ade8db1a4932e653bd85cd5bdee1f5461db9cc
Code-Me <ecabanig22@gmail.com>
2018-11-27 21:39:01 +0800

    fix error, but still has ui error

--- gsfc-pre-main-form.php
+++ gsfc-pre-main-form.php
@@ -83,28 +83,6 @@ require'navbar.php';
             <h2>Generator Set/Fuel Container</h2>
             </div>
 
-<<<<<<< HEAD
-             <div class="row "><h4 class="col-6"><b>Work:</b>&nbsp;
-             <select class="form-control form-control" name="work" id="work">
-                  <option value="Generator Set/Fuel Container">Generator Set/Fuel Container</option>
-                  <option value="Building, Walls, Doors Windows">Building, Walls, Doors Windows</option>
-                  <option value="Power Supply">Power Supply</option>
-                  <option value="Plumbing">Plumbing</option>
-                  <option value="Air-conditioning">Air-conditioning</option>
-                  
-                  
-                </select></h4>
-                              <h4 class="col-6"><b>Campus:</b>&nbsp;
-                                                          <select class="form-control form-control" name="campus" id="campus" readonly>
-                                <?php
-                                require '../api/apiOnly.php';
-                                  $campuses = json_decode($api->fetch_campuses(),true);
-                                  foreach ($campuses as $campus) {
-                                    //$_SESSION['usr_campus'] = 'SAN JUAN';
-=======
->>>>>>> 2b8913ba771bb6afd4feddfe9a34b6b5a7f3c3c5
-
-
              <div class="row">
                                 <h4 class="col-6"><b>Work:</b>&nbsp;
                 <input class="form-control" name="work" value="Generator Set/Fuel Container" readonly>
