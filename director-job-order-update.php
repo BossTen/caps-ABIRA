14456d711a0c92f4ac41e3e50f255c81aa6cb92b
BossTen <tenthequeen@gmail.com>
2018-12-06 08:18:18 +0800

    director job order update title

--- director-job-order-update.php
+++ director-job-order-update.php
@@ -217,7 +217,7 @@ require '../api/dbcon.php';
 <html lang="en">
 
 <head>
-    <title class="no-print">Job Order Form</title>
+    <title class="no-print">Director Job Order View</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
