a83e94755359f311026e5a93620039e58cd13815
Code-Me <ecabanig22@gmail.com>
2018-12-12 00:00:56 +0800

    logic bar chart

--- chart.php
+++ chart.php
@@ -33,6 +33,72 @@ require '../api/dbcon.php';
         $stmt->close();
     $conn->close();
 
+    require '../api/dbcon.php';
+
+    $stmt = $conn->prepare("select
+                           SUM(ResponseTime)
+                           FROM joborder WHERE statusId=6");
+    //$stmt->bind_param('s',$sId);
+    //$sId = isset($_GET['serial'])? $_GET['serial'] : '' ;
+    $stmt->execute();
+    $stmt->bind_result($ResponseTime
+                     );
+
+    $stmt->fetch();
+   // echo 'notcompleted '. $notCompleted;
+
+        $stmt->close();
+    $conn->close();
+
+    require '../api/dbcon.php';
+
+    $stmt = $conn->prepare("select
+                          SUM(AccuracyOfWork)
+                          FROM joborder WHERE statusId=6");
+    //$stmt->bind_param('s',$sId);
+    //$sId = isset($_GET['serial'])? $_GET['serial'] : '' ;
+    $stmt->execute();
+    $stmt->bind_result($AccuracyOfWork
+                     );
+
+    $stmt->fetch();
+
+    $stmt->close();
+    $conn->close();
+
+        require '../api/dbcon.php';
+
+    $stmt = $conn->prepare("select
+                          SUM(Courtesy)
+                          FROM joborder WHERE statusId=6");
+    //$stmt->bind_param('s',$sId);
+    //$sId = isset($_GET['serial'])? $_GET['serial'] : '' ;
+    $stmt->execute();
+    $stmt->bind_result($Courtesy
+                     );
+
+    $stmt->fetch();
+
+    $stmt->close();
+    $conn->close();
+
+
+        require '../api/dbcon.php';
+
+    $stmt = $conn->prepare("select
+                          SUM(QualityOfService)
+                          FROM joborder WHERE statusId=6");
+    //$stmt->bind_param('s',$sId);
+    //$sId = isset($_GET['serial'])? $_GET['serial'] : '' ;
+    $stmt->execute();
+    $stmt->bind_result($QualityOfService
+                     );
+
+    $stmt->fetch();
+
+    $stmt->close();
+    $conn->close();
+
 ?>
 
 <!DOCTYPE html>
@@ -119,25 +185,26 @@ require 'navbar.php';
   var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      labels: ["Response Time", "Accuracy of Work", "Courtesy", "Quality of Service"],
       datasets: [{
-        label: '# of Votes',
-        data: [0, 0, 0, 0, 0, 0],
-        backgroundColor: [
+        label: 'Faculty Feedback',
+        // data: [0, 0, 0, 0, 0, 0],
+          data: [<?php echo $ResponseTime; ?>, <?php echo $AccuracyOfWork; ?>, <?php echo $Courtesy; ?>, <?php echo $QualityOfService; ?>],
+          backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
           'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          // 'rgba(153, 102, 255, 0.2)',
+          // 'rgba(255, 159, 64, 0.2)'
         ],
         borderColor: [
           'rgba(255,99,132,1)',
           'rgba(54, 162, 235, 1)',
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          // 'rgba(153, 102, 255, 1)',
+          // 'rgba(255, 159, 64, 1)'
         ],
         borderWidth: 1
       }]
@@ -164,7 +231,8 @@ require 'navbar.php';
       labels: ["January", "February", "March", "April", "May", "June", "July"],
       datasets: [{
           label: "My First dataset",
-          data: [0, 0, 0, 0, 0, 0, 0],
+          // data: [<?php echo $QualityOfService; ?>, <?php echo $QualityOfService; ?>,, <?php echo $QualityOfService; ?>,, <?php echo $QualityOfService; ?>, 0, 0, 0],
+          data: [<?php echo $ResponseTime; ?>, <?php echo $AccuracyOfWork; ?>, <?php echo $Courtesy; ?>, <?php echo $QualityOfService; ?>, 0, 0, 0],
           backgroundColor: [
             'rgba(105, 0, 132, .2)',
           ],
