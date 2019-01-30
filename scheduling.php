c7c3c5c02207957c208bcac40d1ebd82403bc98c
Code-Me <ecabanig22@gmail.com>
2019-01-30 19:51:18 +0800

    Revert "Update scheduling.php"
    This reverts commit 943ba1a705de4ffe02c3573f997ce4db4e20a53d.

--- scheduling.php
+++ scheduling.php
@@ -109,30 +109,28 @@ require 'navbar-director.php';
                         echo redirectAdmin($serialCode, $statusId, $endOfService);
                         echo redirectAdmin($serialCode, $statusId, $statusName);
                         echo redirectAdmin($serialCode, $statusId, $priorityName);
-                        echo button($serialCode);
-                          echo "</tr>";
-                                  
-                                  
-                                 }
-                              function button($sCode){
-                                return "<td><a href='director-job-order-approval.php?serial=". $sCode. "'><button type='button' class='btn btn-success'>Open</button></a></td>";
-                              }
+                        echo "</tr>";
+
+                                }
+                                break;
+                    }
+
                               function redirectDirector($sCode, $sId, $desc){
                                         return "<td><a href='director-job-order-update.php?serial=". $sCode. "'>" . $desc . "</td>";
                             }
                             function redirectAdmin($sCode, $sId, $desc){
                                         switch($sId){
                                   case 2:
-                                        return "<td>" . $desc . "</td>";
+                                        return "<td><a href='job-order-approved.php?serial=". $sCode. "'>" . $desc . "</td>";
                                         break;
                                   case 5:
-                                        return "<td>" . $desc . "</td>";
+                                        return "<td><a href='job-order-ongoing.php?serial=". $sCode. "'>" . $desc . "</td>";
                                         break;
                                   case 7:
-                                        return "<td>" . $desc . "</td>";
+                                        return "<td><a href='job-order-forinspection.php?serial=". $sCode. "'>" . $desc . "</td>";
                                         break;
                                   default:
-                                        return "<td>" . $desc . "</td>";
+                                        return "<td><a href='job-order.php?serial=". $sCode. "'>" . $desc . "</td>";
                                         break;
                                 }
                             }
