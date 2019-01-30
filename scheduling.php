7e3676d79b5c98391967a4dc1300b93415bd0019
Code-Me <ecabanig22@gmail.com>
2019-01-30 19:50:41 +0800

    Revert "Update scheduling.php"
    This reverts commit ee9ce72412de03e2c328c76a9d2b5acf49f6f2da.

--- scheduling.php
+++ scheduling.php
@@ -109,38 +109,16 @@ require 'navbar-director.php';
                         echo redirectAdmin($serialCode, $statusId, $endOfService);
                         echo redirectAdmin($serialCode, $statusId, $statusName);
                         echo redirectAdmin($serialCode, $statusId, $priorityName);
-                        echo adminButton($serialCode);
+                        echo button($serialCode);
                           echo "</tr>";
                                   
                                   
                                  }
-                        function directorButton($sCode){
-                            return "<td><a href='job-order-approved.php?serial=". $sCode. "'><button type='button' class='btn btn-success'>Open</button></a></td>";
-                        }
-                              function adminButton($sCode,$sId){
+                              function button($sCode){
                                 return "<td><a href='director-job-order-approval.php?serial=". $sCode. "'><button type='button' class='btn btn-success'>Open</button></a></td>";
-                               
-                                switch($sId){
-                                  case 2:
-                                        return "<td><a href='job-order-approved.php?serial=". $sCode. "'><button type='button' class='btn btn-success'>Open</button></a></td>";
-                                        return "<td>" . $desc . "</td>";
-                                        break;
-                                  case 5:
-                                        return "<td><a href='job-order-ongoing.php?serial=". $sCode. "'><button type='button' class='btn btn-success'>Open</button></a></td>";
-                                        return "<td>" . $desc . "</td>";
-                                        break;
-                                  case 7:
-                                        return "<td><a href='job-order-forinspection.php?serial=". $sCode. "'><button type='button' class='btn btn-success'>Open</button></a></td>";
-                                        return "<td>" . $desc . "</td>";
-                                        break;
-                                  default:
-                                        return "<td><a href='job-order.php?serial=". $sCode. "'><button type='button' class='btn btn-success'>Open</button></a></td>";
-                                        return "<td>" . $desc . "</td>";
-                                        break;
-                                }
                               }
                               function redirectDirector($sCode, $sId, $desc){
-                                        return "<td>" . $desc . "</td>";
+                                        return "<td><a href='director-job-order-update.php?serial=". $sCode. "'>" . $desc . "</td>";
                             }
                             function redirectAdmin($sCode, $sId, $desc){
                                         switch($sId){
